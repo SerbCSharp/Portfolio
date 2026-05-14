6069bbee16166302a5691b449f0a20ebece50616
SerbCSharp <SerbCSharp@mail.ru>
2026-05-14 16:30:18 +0300

    DragControls

---
+++ src/meshs.js
@@ -0,0 +1,51 @@
+import * as THREE from "three";
+
+export default function () {
+  const planeGeometry = new THREE.PlaneGeometry();
+  const planeMaterial = new THREE.MeshBasicMaterial({
+    color: "slategrey",
+    side: THREE.DoubleSide,
+  });
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.receiveShadow = true;
+  plane.scale.setScalar(6);
+
+  const portfolio = Frame(-2.3, 0, 0.85, "portfolio");
+  const siteViewAnalysis = Frame(-1.15, 0, 0.85, "");
+  const configurator3D = Frame(0, 0, 0.85, "");
+  const map3D = Frame(1.15, 0, 0.85, "");
+  const airFight = Frame(2.3, 0, 0.85, "");
+
+  const cubeGeometry = new THREE.BoxGeometry();
+  const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: "red",
+  });
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  plane.castShadow = true;
+
+  const group = new THREE.Group();
+  group.add(plane);
+  group.add(portfolio);
+  group.add(siteViewAnalysis);
+  group.add(configurator3D);
+  group.add(map3D);
+  group.add(airFight);
+  group.add(cube);
+  group.rotateX(THREE.MathUtils.degToRad(-90));
+  group.rotateZ(THREE.MathUtils.degToRad(35));
+  group.position.y = -0.82;
+  return group;
+}
+
+function Frame(x, y, z, name) {
+  const portalGeometry = new THREE.PlaneGeometry(1.075, 1.618);
+  const portalMaterial = new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide,
+  });
+  const frame = new THREE.Mesh(portalGeometry, portalMaterial);
+  frame.name = name;
+  frame.castShadow = true;
+  frame.position.set(x, y, z);
+  frame.rotateX(THREE.MathUtils.degToRad(90));
+  return frame;
+}
---
+++ src/portfolio.js
@@ -0,0 +1,78 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import Meshs from "./meshs.js";
+import { DragControls } from "three/examples/jsm/controls/DragControls.js";
+
+// initialize the scene
+const scene = new THREE.Scene();
+
+// add objects to the scene
+const meshs = Meshs();
+scene.add(meshs);
+
+// initialize the camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  200,
+);
+camera.position.z = 3.5;
+
+// initialize the renderer
+const canvas = document.querySelector(".threejs");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// instantiate the controls
+const orbitControls = new OrbitControls(camera, canvas);
+orbitControls.enableDamping = true;
+
+// Объекты, которые можно перетаскивать
+const objects = [meshs.children[1], meshs.children[6]];
+
+const controls = new DragControls(
+  objects, // Список объектов
+  camera, // Камера
+  renderer.domElement, // Элемент холста
+);
+
+// Отключение OrbitControls при начале перетаскивания (важно!)
+controls.addEventListener("dragstart", function (event) {
+  orbitControls.enabled = false;
+});
+
+// Включение OrbitControls обратно
+controls.addEventListener("dragend", function (event) {
+  orbitControls.enabled = true;
+});
+
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+// render the scene
+const renderloop = () => {
+  const currentTime = clock.getElapsedTime();
+  const delta = currentTime - previousTime;
+  previousTime = currentTime;
+
+  // meshs.rotation.y += THREE.MathUtils.degToRad(1) * delta * 20;
+  // meshs.children[2].scale.y = Math.sin(currentTime);
+  // meshs.children[2].position.y = Math.sin(currentTime);
+
+  orbitControls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
+
+renderloop();
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
